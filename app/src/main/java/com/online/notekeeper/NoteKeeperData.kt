92cf0ca8f008ad85ef6a2b25cf44e3173e5ddf9e
abdullahmalimo <abdullahmalimo@gmail.com>
2021-10-25 19:26:48 +0200

    Initial commit

---
+++ app/src/main/java/com/online/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,9 @@
+package com.online.notekeeper
+
+class CourseInfo (val courseId : String,val title: String){
+
+}
+class NoteInfo (var course: CourseInfo, var title: String, var text: String)
+{
+
+}
